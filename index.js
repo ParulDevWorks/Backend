4b7b8956bf219e054c738a9efb3c1292ec9f8212
ParulDevWorks <parulatanncode@gmail.com>
2024-08-01 15:57:55 +0530

    res.json

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const https = require('https');
 const express= require('express');
 const app= express();
 require('dotenv').config();
-const port=process.env.PORT;
+const port=8080;
+
+const name={
+    "nickName":"Naina",
+    "Original":"Parul"
+}
 app.get('/',(req,res)=>{
     res.send('response send');
 })
 app.get('/check',(req,res)=>{
     res.send("<h1>This is express app</h1>");
+})
+app.get('/name',(req,res)=>{
+    res.json(name);
 })
 app.listen(port,(req,res)=>{
     console.log("connected to port");
